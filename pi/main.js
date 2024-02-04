f9c88c574053da177db54ab550a08c110ffff37a
faisalbsh <94784597+faisalbsh@users.noreply.github.com>
2024-02-04 13:29:04 +0300

    cors allow on socket

--- pi/main.js
+++ pi/main.js
@@ -8,8 +8,11 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
+  });
 
 
 io.on('connection', (socket) => {
