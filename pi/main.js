a06109f53c3eb2fc6df506e2604a854e3f58e02a
faisalbsh <94784597+faisalbsh@users.noreply.github.com>
2024-02-04 13:18:46 +0300

    add socket server

--- pi/main.js
+++ pi/main.js
@@ -1,15 +1,37 @@
-var server = new (require("bluetooth-serial-port").BluetoothSerialPortServer)();
+var bluetoothServer = new (require("bluetooth-serial-port").BluetoothSerialPortServer)();
 
 var CHANNEL = 10; // My service channel. Defaults to 1 if omitted.
 var UUID = "38e851bc-7144-44b4-9cd8-80549c6f2912"; // My own service UUID. Defaults to '1101' if omitted
 
-server.on("data", function (buffer) {
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(8081, () => {
+  console.log('listening on *:8081');
+});
+
+
+// bluetooth
+bluetoothServer.on("data", function (buffer) {
     console.log("Received data from client: " + buffer);
 
     // ...
+    io.emit('story', { someProperty: 'value', otherProperty: 'value' }); // This will emit the event to all connected sockets
+
+
 
     console.log("Sending data to the client");
-    server.write(Buffer.from("..."), function (err, bytesWritten) {
+    bluetoothServer.write(Buffer.from("..."), function (err, bytesWritten) {
         if (err) {
             console.log("Error!");
         } else {
@@ -18,7 +40,7 @@ server.on("data", function (buffer) {
     });
 });
 
-server.listen(
+bluetoothServer.listen(
     function (clientAddress) {
         console.log("Client: " + clientAddress + " connected!");
     },
